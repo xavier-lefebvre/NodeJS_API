525c53767b800cef36d18233bb797b65bd95b63e
xavier-lefebvre <xavierlefebvre60@gmail.com>
2022-01-06 16:53:19 +0100

    reservations added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Express integration in index.js
-const express = require('express'); 
-const app = express();
-const parkings = require('./parkings.json');
-const reservations = require('./reservations.json');
+const express = require('express')
+const app = express()
+const parkings = require('./parkings.json')
+const reservations = require('./reservations.json')
 
 //middleware
 app.use(express.json()); // express.json : It parses incoming requests with JSON payloads and is based on body-parser.
@@ -51,53 +51,43 @@ app.delete('/parkings/:id', (req,res) => {
 
 app.get('/parkings/:id/reservations', (req,res) => {
     const id = parseInt(req.params.id);
-    const reservs = reservations.filter(reservs => reservs.parkingId === id); 
-    res.status(200).json(reservs);
+    const byParkingId = reservations.filter(byParkingId => byParkingId.parkingId === id); 
+    res.status(200).json(byParkingId);
 });
 
-app.get('/parking/:id/reservations/:idReservation', (req,res) => {
-    const id = parseInt(req.params.id);
-    const idReservation = parseInt(req.params.idReservation);
-    const reservs = reservations.filter(reservs => reservs.idReservation === idReservation && reservs.parkingId === id); 
-    res.status(200).json(reservs);
+app.get('/parkings/:idp/reservations/:idr', (req,res) => {
+   const idp = parseInt(req.params.idp);
+   const idr = parseInt(req.params.idr);
+   const reserv = reservations.find(reserv => reserv.parkingId === idp && reserv.id === idr); 
+   res.status(200).json(reserv);
 })
 
-// app.get('/reservations', (req,res) => { // get : request to specific resources
-//     // res.send("Liste des parkings"); // send : request to the server
-//     res.status(200).json(reservations); // status : display result of a code ; json : return a json file.
-// });
-
-// app.get('/reservations/:parkingId', (req,res) => {
-//     const parkingId = parseInt(req.params.parkingId);
-//     const reservation = reservations.filter(reservation => reservation.parkingId === parkingId); 
-//     res.status(200).json(reservation);
-// });
-
 // // post request
-// app.post('/reservations', (req,res) => { 
-//     reservations.push(req.body) // push() is an array function from Node.js that is used to add element to the end of an array.
-//     res.status(200).json(reservations)
-// });
+app.post('/parkings/:idp/reservations', (req,res) => { 
+    reservations.push(req.body) // push() is an array function from Node.js that is used to add element to the end of an array.
+    res.status(200).json(reservations)
+});
 
-// // put request
-// app.put('/reservations/:id', (req,res) => {
-//     const id = parseInt(req.params.id)
-//     let reservation = reservation.find(reservation => reservation.id === id)
-//     reservation.name =req.body.name,
-//     reservation.city =req.body.city,
-//     reservation.type =req.body.type,
-//     reservation.clientName = req.body.clientName,
-//     reservation.vehicle = req.body.vehicle,
-//     reservation.licensePlate = req.body.licensePlate,
-//     reservation.checkin = req.body.checkin,
-//     reservation.checkout = req.body.checkout,
-//     res.status(200).json(reservation)
-// })
+// put request
+app.put('/parkings/:idp/reservations/:idr', (req,res) => {
+    const idp = parseInt(req.params.idp);
+    const idr = parseInt(req.params.idr);
+    let reserv = reservations.find(reserv => reserv.parkingId === idp && reserv.id === idr)
+    reserv.parking=req.body.parking,
+    reserv.city =req.body.city,
+    reserv.clientName = req.body.clientName,
+    reserv.vehicle = req.body.vehicle,
+    reserv.licensePlate = req.body.licensePlate,
+    reserv.checkin = req.body.checkin,
+    reserv.checkout = req.body.checkout,
+    res.status(200).json(reserv)
+})
 
-// // delete request
-// app.delete('/reservations/:id', (req,res) => {
-//     const id = parseInt(req.params.id)
-//     let reservation = reservations.find(reservation => reservation.id === id)
-//     reservations.splice(reservations.indexOf(reservation),1)
-//     res.status(200).json(reservations)
-// })
+// delete request
+app.delete('/parkings/:idp/reservations/:idr', (req,res) => {
+    const idp = parseInt(req.params.idp);
+    const idr = parseInt(req.params.idr);
+    let reserv = reservations.find(reserv => reserv.parkingId === idp && reserv.id === idr);
+    reservations.splice(reservations.indexOf(reserv),1)
+    res.status(200).json(reserv)
+})
